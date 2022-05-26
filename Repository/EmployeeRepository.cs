6a93306c02373c1a926f26b85d652e58f5a06e37
DESKTOP-ISQQV62\quoci <quocitspkt@gmail.com>
2022-05-26 08:32:54 +0700

    Add project files.

---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using Contracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+        public IEnumerable<Employee> GetAllEmployees(bool trackChanges) =>FindAll(trackChanges)
+            .OrderBy(c=>c.Name)
+            .ToList();
+    }
+}
---
+++ Service/EmployeeService.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    internal sealed class EmployeeService : IEmployeeService
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        public EmployeeService(IRepositoryManager repository, ILoggerManager
+        logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+        public IEnumerable<EmployeeDto> GetAllEmployees(bool trackChanges)
+        {
+            try
+            {
+                var employees = _repository.Employee.GetAllEmployees(trackChanges);
+                var employeeDto = _mapper.Map<IEnumerable<EmployeeDto>>(employees);
+                return employeeDto;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the { nameof(GetAllEmployees)} service method { ex} ");
+                throw;
+            }
+        }
+    }
+}
+
